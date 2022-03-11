bf4febf8cc6d5d583a7b53e6ad06d24fab2edf6c
tik <sdt@gmael.com>
2022-03-11 17:11:03 +0900

    220130

--- src/board/JChart.java
+++ src/board/JChart.java
@@ -1,5 +1,8 @@
 package board;
 
+import java.awt.BorderLayout;
+import java.awt.event.AdjustmentEvent;
+import java.awt.event.AdjustmentListener;
 import java.io.FileReader;
 import java.io.IOException;
 import java.text.DecimalFormat;
@@ -7,6 +10,7 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 import javax.swing.JPanel;
+import javax.swing.JScrollBar;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
@@ -27,6 +31,7 @@ public class JChart extends JPanel {
 	public JChart() {
 		super();
 		String title = "";
+		setLayout(new BorderLayout());
 		JFreeChart chart;
 		dataSet = new DynamicTimeSeriesCollection(1, 1000, new Second());
 		dataSet.setTimeBase(new Second(Calendar.getInstance().get(Calendar.SECOND),
@@ -41,6 +46,7 @@ public class JChart extends JPanel {
 		axis.setDateFormatOverride(new SimpleDateFormat("ss.SS")); // 타임 포맷
 		ValueAxis vaxis = (ValueAxis) plot.getDomainAxis();
 		vaxis.setFixedAutoRange(10000);
+		add(getScrollBar(vaxis),BorderLayout.SOUTH);
 
 		// y축
 		NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
@@ -69,5 +75,19 @@ public class JChart extends JPanel {
 		newData[0] = (float) temp[1]; // [0] :시간, [1] :가격
 		dataSet.advanceTime(); // x축 시간을 하나추가
 		dataSet.appendData(newData);// 차트에 새로운 y축 데이터 업데이트
+	}
+	
+	private JScrollBar getScrollBar(final ValueAxis domainAxis) {
+		final double r1 = domainAxis.getLowerBound();
+		final double r2 = domainAxis.getUpperBound();
+	    JScrollBar scrollBar = new JScrollBar(JScrollBar.HORIZONTAL, 0, 100, 0, 400);
+	    scrollBar.addAdjustmentListener( new AdjustmentListener() {
+	        public void adjustmentValueChanged(AdjustmentEvent e) {
+	            double x = e.getValue();
+	            domainAxis.setRange(r1+x, r2+x);
+	        }
+	    });
+	    return scrollBar;
+		
 	}
 }
